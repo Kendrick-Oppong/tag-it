dc30fdd2f2fab4bc5c7ded79a9e1b42aab9642b7
Kendrick <kendrick.oppong@amalitech.com>
2025-04-07 00:22:03 +0000

    refactor: update default theme mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       <body className={`text-lg ${bai_Jamjuree.className} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
