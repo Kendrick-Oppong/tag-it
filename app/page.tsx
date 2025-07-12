867f773fd1e9d96c81328ef282d40de86cdc758c
Kendrick <kendrickoppong94@gmail.com>
2025-07-12 12:49:06 +0000

    refactor: update imports and usage to use Component suffix

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import FeaturesComponent from "@/components/home/Features.component";
 import HeroComponent from "@/components/home/Hero.component";
 import TestimonialComponent from "@/components/home/Testimonial.component";
-import Footer from "@/components/shared/footer/Footer.component";
-import Header from "@/components/shared/header/Header.component";
+import FooterComponent from "@/components/shared/footer/Footer.component";
+import HeaderComponent from "@/components/shared/header/Header.component";
 
 export default function Home() {
   return (
     <main className="mt-10 relative">
-      <Header />
+      <HeaderComponent />
       <HeroComponent />
       <FeaturesComponent />
       <TestimonialComponent />
-      <Footer />
+      <FooterComponent />
     </main>
   );
 }
+
